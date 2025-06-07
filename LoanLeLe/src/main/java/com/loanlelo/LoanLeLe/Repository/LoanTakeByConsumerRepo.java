b85ecec7eb1da3216fe0c96582444b49d17fea1b
Anoop kumar <anoopsaw91025@gmail.com>
2025-06-07 19:16:57 +0530

    data base configration

--- LoanLeLe/src/main/java/com/loanlelo/LoanLeLe/Repository/LoanTakeByConsumerRepo.java
+++ LoanLeLe/src/main/java/com/loanlelo/LoanLeLe/Repository/LoanTakeByConsumerRepo.java
@@ -7,6 +7,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.loanlelo.LoanLeLe.DTO.ConsumerDTO.ConsumeProfileLoanData;
 import com.loanlelo.LoanLeLe.Entity.LoanTakenByConsumer;
@@ -15,16 +16,20 @@ import com.loanlelo.LoanLeLe.Enum.LoanbyConsumer.Status;
 @Repository
 public interface LoanTakeByConsumerRepo extends JpaRepository<LoanTakenByConsumer, Long> {
 
-    @Modifying
-    @Query("UPDATE LoanTakenByConsumer l SET l.status =:status WHERE l.id =:id")
-    int adminActivedLoan(@Param("id") Long id, @Param("status") Status status);
+        @Modifying
+        @Query("UPDATE LoanTakenByConsumer l SET l.status =:status WHERE l.id =:id")
+        int adminActivedLoan(@Param("id") Long id, @Param("status") Status status);
 
-    @Modifying
-    @Query("UPDATE LoanTakenByConsumer l SET l.status =:status, l.rejectReason =:rejectReason WHERE l.id =:id")
-    int adminRejectLoan(@Param("id") Long id, @Param("status") Status status,
-            @Param("rejectReason") String rejectReason);
+        @Modifying
+        @Transactional
+        @Query("UPDATE LoanTakenByConsumer l " +
+                        "SET l.status = :status, l.rejectReason = :rejectReason " +
+                        "WHERE l.id = :id")
+        int adminRejectLoan(@Param("id") Long id,
+                        @Param("status") Status status,
+                        @Param("rejectReason") String rejectReason);
 
-    @Query("SELECT new com.loanlelo.LoanLeLe.DTO.ConsumerDTO.ConsumeProfileLoanData(l.loanAmount, l.status, l.loanPurpose, l.interestRate, l.loanTerm, l.monthlyPayment)"
-            + "FROM LoanTakenByConsumer l " + "JOIN l.consumer c" + " WHERE c.email =:email")
-    List<ConsumeProfileLoanData> makeComsuerProfile(@Param("email") String email);
+        @Query("SELECT new com.loanlelo.LoanLeLe.DTO.ConsumerDTO.ConsumeProfileLoanData(l.loanAmount, l.status, l.loanPurpose, l.interestRate, l.loanTerm, l.monthlyPayment)"
+                        + "FROM LoanTakenByConsumer l " + "JOIN l.consumer c" + " WHERE c.email =:email")
+        List<ConsumeProfileLoanData> makeComsuerProfile(@Param("email") String email);
 }
